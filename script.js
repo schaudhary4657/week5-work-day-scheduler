efb9603837185a1c042c457604dcdc33b5027671
Sapana Chaudhary <sapana.chy23@gmail.com>
2020-04-03 22:40:18 -0700

    update code of all files

---
+++ script.js
@@ -0,0 +1,164 @@
+$(document).ready(function() {
+    // displays time from the moment on the page
+    var date = moment().format("dddd MMMM DD YYYY");
+    var dateHour24 = moment().format("H");
+    var dateHour12 = moment().format("h");
+    var test = false;
+
+    if (test) {
+        dateHour24 = 13;
+        dateHour12 = 1;
+    }
+
+    var $displayDate = $("#currentDay")
+    $displayDate.text(date);
+
+    var saveIcon = "https://fontawesome.com/icons/save?style=regular";
+
+    // getting stored data from local storage and update the array with retrieved data
+    var storedSchedule = JSON.parse(localStorage.getItem("storedSchedule"));
+
+    if (test) { console.log(storedSchedule); }
+
+    if (storedSchedule !== null) {
+        textarea = storedSchedule;
+    }
+    else {
+        textarea = new Array(9);
+        textarea[2] = "Event that already happened";
+        textarea[3] = "Current Hour";
+    }
+
+    if (test) { console.log("full array of planned text", textarea); }
+    
+    // building scheduler with fix set of hours
+    var $scheduleDiv = $("#daily-schedule");
+    $scheduleDiv.empty();
+
+    if (test) { console.log("current time", dateHour12); }
+
+    for (var hour = 9; hour <= 17; hour++) {
+        var index = hour - 9;
+        
+        // building rows
+        var $rowDiv = $("<div>");
+        $rowDiv.addClass("row");
+        $rowDiv.addClass("schedulerRow");
+        $rowDiv.attr("hour-index", hour);
+      
+        var $column2TimeDiv = $("<div>");
+        $column2TimeDiv.addClass("col-md-2");
+      
+        // creating element that contains time
+        var $timeBlockSpan = $("<span>");
+        $timeBlockSpan.attr("class", "time-block");
+        
+        // formatting hours to display
+        var displayHour = 0;
+        var ampm = "";
+        if (hour > 12) {
+          displayHour = hour - 12;
+          ampm = "pm";
+        }
+        else {
+          displayHour = hour;
+          ampm = "am";
+        }
+        
+        // display timeBlock with time
+        $timeBlockSpan.text(`${displayHour} ${ampm}`);
+    
+        $rowDiv.append($column2TimeDiv);
+        $column2TimeDiv.append($timeBlockSpan);
+    
+        // building row components
+        var $dailyScheduleSpan = $("<input>");
+    
+        $dailyScheduleSpan.attr("id",`input-${index}`);
+        $dailyScheduleSpan.attr("hour-index", index);
+        $dailyScheduleSpan.attr("type", "text");
+        $dailyScheduleSpan.attr("class", "dailySchedule");
+        $dailyScheduleSpan.val(textarea[index]);
+        
+        // creating column width
+        var $column9InputDiv = $("<div>");
+        $column9InputDiv.addClass("col-md-9");
+    
+        // adding column width and row component to row
+        $rowDiv.append($column9InputDiv);
+        $column9InputDiv.append($dailyScheduleSpan);
+    
+        // building save portion of row
+        var $column1SaveDiv = $("<div>");
+        $column1SaveDiv.addClass("col-md-1");
+    
+        var $saveBtn = $("<i>");
+        $saveBtn.attr("id", `saveid-${index}`);
+        $saveBtn.attr("save-id", index);
+        $saveBtn.attr("class", "far fa-save saveIcon");
+        
+        // adding column width and row component to row
+        $rowDiv.append($column1SaveDiv);
+        $column1SaveDiv.append($saveBtn);
+    
+        // setting row color based on time
+        updateRowColor($rowDiv, hour);
+        
+        // adding row to scheduler container
+        $scheduleDiv.append($rowDiv);
+    };
+
+    // function to update color of row
+    function updateRowColor ($hourRow, hour) {
+        if (test) { console.log("rowColor ", dateHour24, hour); }
+
+        if (hour < dateHour24) {
+            if (test) { console.log("lessThan"); }
+            $hourRow.css("background-color","lightgrey");
+            // $hourRow.attr("class", "past");
+        }
+        else if ( hour > nowHour24) {
+            if (test) { console.log("greaterthan"); }
+            $hourRow.css("background-color","lightgreen");
+            // $hourRow.attr("class", "future");
+        }
+        else {
+            if (test) { console.log("equal"); }
+            $hourRow.css("background-color","lightred");
+            // $hourRow.attr("class", "present");
+        }
+    };
+
+    // editing on schedule area and saves the data to local storage
+    $(document).on("click", "i", function(event) {
+        event.preventDefault();  
+
+        if (test) { console.log('click pta before ' + textarea); }
+
+        var $index = $(this).attr("save-id");
+
+        var inputId = "#input-" + $index;
+        var $value = $(inputId).val();
+
+        textarea[$index] = $value;
+
+
+        if (test) { console.log("value", $value); }
+        if (test) { console.log("index" , $index); }
+        if (test) { console.log("click pta after " + textarea); }
+
+        $(`#saveid-${$index}`).removeClass("shadowPulse");
+        localStorage.setItem("storedSchedule", JSON.stringify(textarea));
+    });
+
+    // function save button on change of input, check save button and add shadow pulse class
+    $(document).on("change", "input", function(event) {
+        event.preventDefault();  
+        if (test) { console.log("onChange"); }
+        if (test) { console.log("id", $(this).attr("hour-index")); }
+
+        var i = $(this).attr("hour-index");
+
+        $(`#saveid-${i}`).addClass("shadowPulse");
+    });
+});
